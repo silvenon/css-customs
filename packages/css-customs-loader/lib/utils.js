0cea8a2600c2649392facf37990750ca83832e45
Matija Marohnić <matija.marohnic@gmail.com>
2020-01-28 23:01:46 +0100

    Use library for getting default export from string

--- packages/css-customs-loader/lib/utils.js
+++ packages/css-customs-loader/lib/utils.js
@@ -1,5 +1,3 @@
-const Module = require('module')
-
 const isLoader = (loader, loaderName) =>
   loader.path.match(new RegExp(`\\b${loaderName}\\b`))
 
@@ -13,18 +11,8 @@ const findNextLoader = (self, loaderName) =>
     .slice(self.loaderIndex + 1)
     .find(loader => isLoader(loader, loaderName))
 
-// https://github.com/webpack/webpack.js.org/issues/1268#issuecomment-313513988
-const exec = (code, filename, context) => {
-  const module = new Module(filename, this)
-  module.paths = Module._nodeModulePaths(context)
-  module.filename = filename
-  module._compile(code, filename)
-  return module.exports
-}
-
 module.exports = {
   isLoader,
   findPreviousLoader,
   findNextLoader,
-  exec,
 }
